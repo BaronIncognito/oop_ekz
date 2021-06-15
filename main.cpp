ef04aacae14551787aca2eb126a98f710640bb24
danielmalakhov <danya.malakhov@gmail.com>
2021-06-15 10:53:14 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using std::vector;
+
+void longestAscendingSubsequence(const vector<char> &v) {
+    int size = v.size();
+    int max = 0, current = 1;
+    char second = v[0], first;
+    for (int i = 1; i < size; i++) {
+        if (v[i] > v[i - 1]) {
+            current++;
+        }
+        else if (current > max) {
+            max = current;
+            second = v[i - current];
+            first = i > current ? v[i - current - 1] : -1;
+            current = 1;
+        }
+    }
+    std::cout << max << " " << first;
+    if (second >= 0) {
+        std::cout << second;
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    char data[] = "abirvalghjklm";
+    vector<char> v;
+    v.insert(v.end(), data, data + sizeof(data) / sizeof(char));
+    longestAscendingSubsequence(v);
     return 0;
 }
