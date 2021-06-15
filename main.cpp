34aefc94c4789f1ff020554f06c49ea901bd6f11
danielmalakhov <danya.malakhov@gmail.com>
2021-06-15 10:37:22 +0300

    lAiS

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <vector>
+using std::vector;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void longestAscendingSubsequence(const vector<char> &v) {
+    int size = v.size();
+    int max = 0, current = 0;
+    char second = v[0], first;
+    for (int i = 1; i < size; i++) {
+        if (v[i] > v[i - 1]) {
+            current++;
+        }
+        else if (current > max) {
+            max = current;
+            second = v[i - current];
+            first = i > current ? v[i - current - 1] : -1;
+            current = 1;
+        }
+    }
+    cout << max << " " << first;
+    if (second >= 0) {
+        cout << second;
+    }
+    cout << endl;
 }
